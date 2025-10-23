f659d46cf03b59b8e3843e10123e0fc78940cd64
Andy <andy774823@gmail.com>
2025-10-23 15:01:05 +0800

    change m4 to place

--- app/m4/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-export default function M4Page() {
-  return (
-    <>
-      <h1>m4 page</h1>
-    </>
-  );
-}
---
+++ app/place/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function PlacePage() {
+  return (
+    <>
+      <h1>Place</h1>
+    </>
+  );
+}
