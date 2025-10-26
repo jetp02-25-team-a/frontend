678bb1c39d554898a73dffdc982ae6fa8ab20be4
yukion614 <34601876+yukion614@users.noreply.github.com>
2025-10-26 15:50:50 +0800

    新增node元件 ,增加至page

--- app/grabgroup/group-itinerart-detal/page.tsx
+++ app/grabgroup/group-itinerart-detal/page.tsx
@@ -5,18 +5,93 @@ import {
 } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import DayCard from './_components/DayCard';
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import NodeCard from './_components/NodeCard';
+import { addTimeWrap } from '../utils';
 
 const data = [
-  { id: 1, date: '2025-10-25', start_time: '09:00:00' },
-  { id: 2, date: '2025-10-26', start_time: '10:00:00' },
+  {
+    id: 1,
+    date: '2025-10-25',
+    start_time: '09:00:00',
+    nodes: [
+      {
+        id: 1,
+        image: '/image.png',
+        title: '台北一日遊',
+        duration_minute: '01:15:00',
+        address: '110臺北市信義區市府路45 號',
+      },
+      {
+        id: 2,
+        image: '/image.png',
+        title: '台北2日遊',
+        duration_minute: '01:15:00',
+        address: '110臺北市信義區市府路45 號',
+      },
+      {
+        id: 3,
+        image: '/image.png',
+        title: '台北2日遊',
+        duration_minute: '06:00:00',
+        address: '110臺北市信義區市府路45 號',
+      },
+    ],
+  },
+  {
+    id: 2,
+    date: '2025-10-26',
+    start_time: '10:00:00',
+    nodes: [
+      {
+        id: 1,
+        image: '/image.png',
+        title: '台北一日遊',
+        duration_minute: '01:15:00',
+        address: '110臺北市信義區市府路45 號',
+      },
+      {
+        id: 2,
+        image: '/image.png',
+        title: '台北2日遊',
+        duration_minute: '01:15:00',
+        address: '110臺北市信義區市府路45 號',
+      },
+      {
+        id: 3,
+        image: '/image.png',
+        title: '台北2日遊',
+        duration_minute: '06:00:00',
+        address: '110臺北市信義區市府路45 號',
+      },
+    ],
+  },
   { id: 3, date: '2025-10-27', start_time: '09:30:00' },
   { id: 4, date: '2025-10-28', start_time: '09:30:00' },
   { id: 5, date: '2025-10-29', start_time: '09:30:00' },
 ];
+
+interface MyNode {
+  id: number;
+  image: string;
+  title: string;
+  duration_minute: string;
+  address: string;
+  start_time?: string;
+  end_time?: string;
+}
+
+interface DayWithNodes {
+  id: number;
+  date: string;
+  start_time: string;
+  nodes?: MyNode[];
+}
+
 export default function GroupItineraryDetalPage() {
   const scrollRef = useRef<HTMLDivElement>(null);
 
+  //處理滑動
   const scroll = (direction: 'pre' | 'next') => {
     if (!scrollRef) return;
     const scrollAmount = 90;
@@ -26,11 +101,33 @@ export default function GroupItineraryDetalPage() {
     });
   };
 
+  const [days, setDays] = useState<DayWithNodes[]>([]);
+
+  useEffect(() => {
+    const newData = data.map((day) => ({
+      ...day,
+      nodes: day.nodes?.reduce(
+        (acc, node, idx) => {
+          const start_time = idx === 0 ? day.start_time : acc[idx - 1].end_time;
+
+          const end_time = addTimeWrap(start_time, node.duration_minute);
+
+          acc.push({ ...node, start_time, end_time });
+          return acc; // 給下一輪使用
+        },
+        [] as (MyNode & { start_time: string; end_time: string })[]
+      ),
+    }));
+
+    setDays(newData);
+  }, []);
+
   return (
     <>
       {/* map_area */}
-      <div className="grid grid-cols-[40%_60%]">
-        <div className="bg-gray-200 p-[15px] space-y-3.5">
+      <div className="grid grid-cols-[40%_60%] h-screen">
+        {/* area_zone */}
+        <div className="bg-gray-200 p-[15px] space-y-3.5 overflow-scroll">
           <h3 className="text-3xl">行程</h3>
           {/* date_bar area */}
           <div className="flex w-full ">
@@ -65,7 +162,38 @@ export default function GroupItineraryDetalPage() {
               新增
             </button>
           </div>
+          {/* 顯示node區域 */}
+          <div className="flex flex-col items-center gap-3.5">
+            {days.map((day, index) => {
+              return (
+                <>
+                  <div className="w-full">
+                    <h3 className="text-start text-[24px]">{`第${index + 1}天`}</h3>
+                    <p className="text-start text-base">{day.date}</p>
+                  </div>
+
+                  {day.nodes?.map((node, index) => {
+                    return (
+                      <>
+                        <NodeCard
+                          key={index}
+                          image={node.image}
+                          duration_minute={node.duration_minute}
+                          title={node.title}
+                          address={node.address}
+                          start_time={node.start_time?.slice(0, 5)}
+                          end_time={node.end_time?.slice(0, 5)}
+                        />
+                        <div className="bg-gray-600 w-1 h-[43px]"></div>
+                      </>
+                    );
+                  })}
+                </>
+              );
+            })}
+          </div>
         </div>
+        {/* map_zone */}
         <div className="bg-amber-700">bbb</div>
       </div>
     </>
--- app/grabgroup/utils.ts
+++ app/grabgroup/utils.ts
@@ -20,6 +20,21 @@ export function addTimeWrap(baseTime: string, addTime: string) {
     .map((v) => String(v).padStart(2, '0'))
     .join(':');
 }
+//減去時間 沒有秒
+export function reduceTimeWrap(baseTime: string, addTime: string) {
+  const [h1, m1] = baseTime.split(':').map(Number);
+  const [h2, m2] = addTime.split(':').map(Number);
+  let totalSeconds = h1 * 3600 + m1 * 60 - (h2 * 3600 + m2 * 60);
+
+  // 取 24 小時內的餘數
+  totalSeconds %= 86400;
+
+  const hours = Math.floor(totalSeconds / 3600);
+  const minutes = Math.floor((totalSeconds % 3600) / 60);
+
+  return [hours, minutes].map((v) => String(v).padStart(2, '0')).join(':');
+}
+
 //YYYY-MM-DD to MM月DD日
 export function formatDateToChinese(dateStr: string) {
   const date = new Date(dateStr);
@@ -27,3 +42,18 @@ export function formatDateToChinese(dateStr: string) {
   const day = date.getDate();
   return `${month}月${day}日`;
 }
+
+// 轉換成時間小時到分
+export function getTimeCost(time?: string) {
+  if (!time) return;
+  const [sh, sm] = time.split(':').map(Number);
+
+  const timeMinutes = sh * 60 + sm;
+
+  const hours = Math.floor(timeMinutes / 60);
+  const minutes = timeMinutes % 60;
+
+  return `${hours}小時${minutes}分`;
+}
+
+//
