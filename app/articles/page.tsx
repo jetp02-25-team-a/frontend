3bfc7d0aff470b68744953b2e1e5680e77d8c4c3
Hanna-Wu18 <wu.kasome@gmail.com>
2025-10-29 16:09:29 +0800

    add-ArticlePage

--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -1,9 +1,26 @@
 'use client';
 
-export default function ArticlesPage() {
+import React, { useState, useEffect } from 'react';
+import HeroImage from '@/components/HeroImage';
+import HeroSection from '@/components/HeroSection';
+import IntroText from '@/components/IntroText';
+import SidebarAction from '@/components/SidebarAction';
+import DestinationGrid from '@/components/DestinationGrid';
+
+export default function ArticlePage() {
   return (
-    <>
-      <div>Articles Page</div>
-    </>
+    <main>
+      <div className="flex-1">
+        <HeroImage />
+        {/* <HeroSection /> */}
+        <div className="text-center mt-6 text-xl font-bold">
+          你的旅程，不只是回憶——也是靈感的起點！
+        </div>
+        <div className="flex">
+          <SidebarAction />
+          <DestinationGrid />
+        </div>
+      </div>
+    </main>
   );
 }
