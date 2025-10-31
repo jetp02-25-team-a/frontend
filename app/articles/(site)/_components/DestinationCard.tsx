afcd5258bfb21c67928cb2c3d5862cdbcdeca8fa
Hanna-Wu18 <wu.kasome@gmail.com>
2025-10-31 13:36:22 +0800

    DestinationCard

--- app/articles/(site)/_components/DestinationCard.tsx
+++ app/articles/(site)/_components/DestinationCard.tsx
@@ -1,7 +1,20 @@
 'use client';
 import Image from 'next/image';
 
-export default function DestinationCard({ image, title, description }: {
+function isValidUrl(url: string): boolean {
+  try {
+    new URL(url);
+    return true;
+  } catch {
+    return false;
+  }
+}
+
+export default function DestinationCard({
+  image,
+  title,
+  description,
+}: {
   image: string;
   title: string;
   description: string;
@@ -9,7 +22,7 @@ export default function DestinationCard({ image, title, description }: {
   return (
     <div className="bg-white shadow rounded overflow-hidden">
       <Image
-        src={image}
+        src={isValidUrl(image) ? image : "/istockphoto-1209191587-612x612.jpg"}
         alt={title}
         width={30}
         height={30}
@@ -23,3 +36,43 @@ export default function DestinationCard({ image, title, description }: {
     </div>
   );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 'use client';
+// import Image from 'next/image';
+
+// export default function DestinationCard({ image, title, description }: {
+//   image: string;
+//   title: string;
+//   description: string;
+// }) {
+//   return (
+//     <div className="bg-white shadow rounded overflow-hidden">
+//       <Image
+//         src={isValidUrl(image) ? image : "/public/istockphoto-1209191587-612x612.jpg"}
+
+//         alt={title}
+//         width={30}
+//         height={30}
+//         className="object-cover w-full"
+//       />
+//       <div className="p-4">
+//         <h3 className="text-lg font-semibold">{title}</h3>
+//         <p className="text-sm text-gray-600 mt-2">{description}</p>
+//         <button className="mt-4 text-blue-600 hover:underline">查看</button>
+//       </div>
+//     </div>
+//   );
+// }
--- app/articles/(site)/_components/HeroImage.tsx
+++ app/articles/(site)/_components/HeroImage.tsx
@@ -11,7 +11,7 @@ export default function HeroSection() {
   return (
     <>
      <div className="relative w-full h-[500px] bg-white-200 ">
-    <Image src={'/image/BANNER-3.jpg'} alt='' fill style={{zIndex:'-1'}}/>
+    <Image src={'/images/BANNER-3.jpg'} alt='' fill style={{zIndex:'-1'}}/>
       <h1 className=" text-white  text-6xl font-bold relative pt-40 text-center">
         台灣之美，等你親自體驗
       </h1>
