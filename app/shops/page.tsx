5c03b1812891b73c5b3cd9d55f40bb501cb52888
tav <wcaka78@gmail.com>
2025-11-03 15:39:40 +0800

    拿取商品列表

--- app/shops/page.tsx
+++ app/shops/page.tsx
@@ -1,9 +1,38 @@
 'use client';
 
+import { API_SERVER } from '../config/api-path';
+import { useState, useEffect } from 'react';
+import { ApiResponse } from './_interfaces/data';
+
+const rProductinit: ApiResponse = {
+  success: false,
+  data: [],
+};
+
 export default function M6Page() {
+  //資料存放
+  const [rProduct, setrProduct] = useState(rProductinit);
+
+  //資料拿取
+  const readProduct = async () => {
+    try {
+      const res = await fetch(`${API_SERVER}/recommend`);
+      const data = await res.json();
+      setrProduct(data.data);
+    } catch (e) {}
+  };
+
+  //進入網頁讀取
+  useEffect(() => {
+    readProduct();
+  }, []);
   return (
     <>
-      <h1>m6 page</h1>
+      <ul>
+        {rProduct.data.map((item) => {
+          return <li key={item.id}>{item.productName}</li>;
+        })}
+      </ul>
     </>
   );
 }
