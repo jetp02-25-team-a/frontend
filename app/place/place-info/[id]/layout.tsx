8ba44b71403bbc3621caa0ac17952006479bfc29
Andy <andy774823@gmail.com>
2025-11-05 09:39:43 +0800

    更新layout

--- app/place/place-info/[id]/layout.tsx
+++ app/place/place-info/[id]/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
-import '../../styles/globals.css';
+import '@/styles/globals.css';
+import { places } from '@/app/place/lib/fixtures';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
