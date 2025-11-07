2999deb421f2d94f2ec6090550e4b07845305e18
tav <wcaka78@gmail.com>
2025-11-07 16:08:47 +0800

    商城首頁

--- app/shops/_components/productCard.tsx
+++ app/shops/_components/productCard.tsx
@@ -21,26 +21,46 @@ export default function ProductCard({
   variantId,
 }: ProductCardProps) {
   const { addToCart } = useCart();
+  const [isHovered, setIsHovered] = useState(false);
   const itemToCart = (itemId: number, variantId: number) => {
     addToCart(itemId, variantId);
     return;
   };
   return (
     <>
-      <div className="bg-white rounded-xl overflow-hidden m-2">
-        <div className="relative w-full pt-[75%] ">
+      <div
+        className="bg-[#F8D28C] rounded-xl overflow-hidden m-2"
+        onMouseEnter={() => setIsHovered(true)} // 滑鼠進入
+        onMouseLeave={() => setIsHovered(false)} // 滑鼠離開
+      >
+        <div className="relative w-full pt-[75%] mb-2">
           <Image
             src={pictureURL}
             alt="產品圖片"
             fill
             style={{ objectFit: 'cover' }}
           ></Image>
+          <div
+            className={`absolute inset-0 bg-black transition-opacity duration-300 ${
+              isHovered ? 'opacity-30' : 'opacity-0'
+            }`}
+          ></div>
+
+          <div
+            className={`absolute inset-0 flex items-center justify-center transition-opacity duration-300 ${
+              isHovered ? 'opacity-100 z-10' : 'opacity-0 pointer-events-none'
+            }`}
+          >
+            <button
+              onClick={() => itemToCart(productId, variantId)}
+              className="bg-white text-[#F8D28C] font-bold py-2 px-4 rounded-full shadow-xl hover:bg-gray-100 transition-colors duration-200"
+            >
+              加入購物車
+            </button>
+          </div>
         </div>
-        <p>{productName}</p>
-        <p>{price}</p>
-        <button onClick={() => itemToCart(productId, variantId)}>
-          加入購物車
-        </button>
+        <p className="mb-2 ml-3">{productName}</p>
+        <p className="mb-2 ml-3">NT${price}</p>
       </div>
     </>
   );
--- app/shops/page.tsx
+++ app/shops/page.tsx
@@ -7,49 +7,127 @@ import { useCart } from '../../hooks/use-Cart';
 import Slider from 'react-slick';
 import ShoppingCart from './_components/shoppingCart';
 import ProductCard from './_components/productCard';
+import { GrLinkNext } from 'react-icons/gr';
 
-const rProductinit: ApiResponse = {
+const ProductInit: ApiResponse = {
   success: false,
   data: [],
 };
 
+type TabKey = 'hot' | 'new' | 'recommend';
+
+function NextArrow(props: any) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: 'block' }}
+      onClick={onClick}
+    ></div>
+  );
+}
+
+function PrevArrow(props: any) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: 'block' }}
+      onClick={onClick}
+    />
+  );
+}
+
 export default function M6Page() {
-  const { cart, addToCart } = useCart();
+  const { cart } = useCart();
   //資料存放
-  const [rProduct, setrProduct] = useState(rProductinit);
+  const [rProduct, setrProduct] = useState(ProductInit);
+  const [nProduct, setnProduct] = useState(ProductInit);
+  const [hProduct, sethProduct] = useState(ProductInit);
+
+  const [activeTab, setActiveTab] = useState<TabKey>('hot');
 
   //資料拿取
-  const getProduct = async () => {
+  const getrProduct = async () => {
     try {
       const res = await fetch(`${API_SERVER}/recommend`);
       const data = await res.json();
       setrProduct(data);
     } catch (e) {}
   };
-
-  const settings = {
-    dots: false, // 導航點
-    infinite: true, // 無限循環
-    speed: 500,
-    slidesToShow: 4, // 一次顯示 4 張卡片
-    slidesToScroll: 1, // 每次捲動 1 張卡片
-    arrows: true,
+  const getnProduct = async () => {
+    try {
+      const res = await fetch(`${API_SERVER}/new`);
+      const data = await res.json();
+      setnProduct(data);
+    } catch (e) {}
+  };
+  const gethProduct = async () => {
+    try {
+      const res = await fetch(`${API_SERVER}/hot`);
+      const data = await res.json();
+      sethProduct(data);
+    } catch (e) {}
   };
 
-  const itemToCart = (itemId: number, variantId: number) => {
-    addToCart(itemId, variantId);
-    return;
+  const settings = {
+    dots: true, // 導航點
+    infinite: true, // 無限循環
+    speed: 500,
+    slidesToShow: 6, // 每次顯示
+    slidesToScroll: 6, // 每次捲動
+    arrows: true,
+    draggable: false,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+  };
+
+  const GridSettings = {
+    dots: false,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 6,
+    slidesToScroll: 0,
+    rows: 4,
+    slidesPerRow: 1,
+    arrows: true,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+    swipe: false,
+    draggable: false,
+  };
+
+  const tabs: { key: TabKey; name: string }[] = [
+    { key: 'hot', name: '熱門商品' },
+    { key: 'new', name: '最新商品' },
+    { key: 'recommend', name: '推薦商品' },
+  ];
+
+  const displayData = () => {
+    switch (activeTab) {
+      case 'hot':
+        return hProduct.data;
+      case 'new':
+        return nProduct.data;
+      case 'recommend':
+        return rProduct.data;
+      default:
+        return rProduct.data;
+    }
   };
 
   //進入網頁讀取
   useEffect(() => {
-    getProduct();
+    getrProduct();
+    getnProduct();
+    gethProduct();
   }, []);
   return (
     <>
-      <div className="bg-[#FBE7C1] w-full">
-        <div className="w-4/5  mx-auto">
-          <Slider {...settings} className="p-4 ">
+      <div className="bg-[#FBE7C1] w-full  pt-8 pb-8">
+        <div className="w-7/8  mx-auto h-90 ">
+          <h1 className="text-4xl font-bold text-center mb-8">推薦商品</h1>
+          <Slider {...settings}>
             {rProduct.data.map((item) => {
               return (
                 <div key={item.id} className="slider-container w-90">
@@ -65,6 +143,46 @@ export default function M6Page() {
             })}
           </Slider>
         </div>
+        <h1 className="text-4xl font-bold text-center mt-16">更多商品</h1>
+        <div className="flex justify-center mb-8 space-x-4 mt-8">
+          {tabs.map((tab) => (
+            <button
+              key={tab.key}
+              onClick={() => setActiveTab(tab.key)}
+              className={`
+                  text-3xl font-bold p-2 transition-colors duration-200 
+                  ${
+                    activeTab === tab.key
+                      ? 'text-black border-b-4 border-black' // 選中樣式
+                      : 'text-gray-500 hover:text-gray-700' // 未選中樣式
+                  }
+                `}
+            >
+              {tab.name}
+            </button>
+          ))}
+        </div>
+        <div className="w-7/8  mx-auto h-fit ">
+          <Slider {...GridSettings}>
+            {displayData().map((item) => {
+              return (
+                <div key={item.id} className="slider-container w-90">
+                  <ProductCard
+                    productName={item.productName}
+                    price={item.ProductVariants[0].price}
+                    pictureURL={item.ProductPics[0].src}
+                    productId={item.id}
+                    variantId={item.ProductVariants[0].id}
+                  />
+                </div>
+              );
+            })}
+          </Slider>
+        </div>
+        <h1 className="text-4xl font-bold text-center mt-16 flex items-center justify-center space-x-2 ">
+          所有商品
+          <GrLinkNext />
+        </h1>
         <ShoppingCart totalItems={cart.totalItems} />
       </div>
     </>
