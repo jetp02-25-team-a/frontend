adbbaa8230e50194485ca8b1051b9e13727f9975
yukion614 <34601876+yukion614@users.noreply.github.com>
2025-11-13 06:43:25 +0800

    好友推薦卡片內容顯示修正 元件化

--- app/member/_components/friend-card.tsx
+++ app/member/_components/friend-card.tsx
@@ -14,7 +14,7 @@ export default function FriendCard({ avatar, name, address }: FriendCard) {
     <div className="bg-white px-6 py-4 rounded-2xl flex items-center gap-6 border border-gray-200 shadow-[0_8px_20px_rgba(0,0,0,0.2)]">
       <div className="w-[100px] h-[100px] relative shrink-0 overflow-hidden rounded-full">
         <Image
-          src={`/${avatar}`}
+          src={avatar}
           alt=""
           fill
           className="object-cover shadow-[0_4px_8px_rgba(0,0,0,0.4)]"
---
+++ app/member/user-info/_components/friend-recommend.tsx
@@ -0,0 +1,80 @@
+'use client';
+
+import React, { useEffect, useState } from 'react';
+import FriendCard from '../../_components/friend-card';
+import { useFetch } from '@/hooks/useFetch';
+import { API_SERVER } from '../../../../config/api-path';
+import { useAuth } from '@/hooks/use-Auth';
+import { AVATAR_PATH } from '../../../config/image-path';
+
+// interface Friend {
+//   avatar: string;
+//   user_name: string;
+//   address: string;
+// }
+
+// interface FriendRecommendProps {
+//   friend_data: Friend[];
+// }
+
+export default function FriendRecommend() {
+  const [friendRecommend, setFriendRecommend] = useState<any[]>([]);
+
+  const { user } = useAuth();
+  const limit = 2;
+  const url = `${API_SERVER}/friendships/similar-experience?limit=${limit}`;
+  const { data, loading, error, refetch } = useFetch(url);
+
+  useEffect(() => {
+    refetch();
+  }, [user]);
+
+  useEffect(() => {
+    if (data && data.success) {
+      setFriendRecommend(data.data);
+    } else if (data && !data.success) {
+      console.error('API 回應失敗:', data);
+    }
+  }, [data]);
+
+  if (loading) {
+    return (
+      <div className="p-2.5 space-y-2.5">
+        <p>載入中...</p>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="p-2.5 space-y-2.5">
+        <p className="text-red-500">載入失敗: {error.message}</p>
+        <button
+          onClick={() => refetch()}
+          className="px-4 py-2 bg-blue-500 text-white rounded"
+        >
+          重新載入
+        </button>
+      </div>
+    );
+  }
+
+  return (
+    <div className="p-2.5 space-y-2.5">
+      {friendRecommend && friendRecommend.length > 0 ? (
+        friendRecommend.map((friend: any, index: number) => (
+          <FriendCard
+            key={index}
+            avatar={
+              `${AVATAR_PATH}${friend.user.avatar}` || '/avatar_default.png'
+            }
+            name={friend.user.nickname || friend.fullName}
+            address={friend.overlappedAttractions[0].name || '未提供地址'}
+          />
+        ))
+      ) : (
+        <p>沒有推薦好友</p>
+      )}
+    </div>
+  );
+}
